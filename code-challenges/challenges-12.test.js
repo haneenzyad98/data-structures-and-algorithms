f9139a0c15840250e4526429403937154f87731a
haneenzyad98 <haneenzyad98@gmail.com>
2021-04-06 09:11:30 +0300

    ch12

--- code-challenges/challenges-12.test.js
+++ code-challenges/challenges-12.test.js
@@ -2,63 +2,52 @@
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1 - Review
-
 Write a function that finds maximum value in an array
 using the 'reduce' method.
-
 E.g. [4,2,7,5,9,2] -> 9
 ------------------------------------------------------------------------------------------------ */
 const maxInArray = (arr) => {
   // Solution code here...
- let ini=0;
- arr.reduce((accom,value)=>{
-   if(accom>value){
-     return ini=accom;
-   }else{
-     return ini=value;
-   }
- },0)
- return ini;
+  let num =0;
+  arr.reduce((acc, val)=>{
+    if(acc>val){
+      return num = acc;
+    }else{
+      return num = val;
+    }
+  },0)
+  return num;
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
-
-Write a function named findMax that takes in a matrix
- of positive numbers and returns the number with the highest 
- value.
-
+Write a function named findMax that takes in a matrix of positive numbers and returns the number with the highest value.
 For example: 
 [
   [1, 3, 4, 5],
   [4, 5, 6],
   [23, 5, 5]
 ]
-
 return: 23
 ------------------------------------------------------------------------------------------------ */
 const findMax = (matrix) => {
   // Solution code here...
-  let x=[];
-  matrix.array.forEach(element => {
-    x.push(maxInArray(element));
+  let localArr = [];
+  matrix.forEach(element => {
+    localArr.push(maxInArray(element));
   });
-  return maxInArray(x)
+  return maxInArray(localArr);
 };
-
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
-
 Write a function named totalSum that takes in a matrix of numbers and returns the totalSum of all the numbers.
-
 For example: 
 [
   [1, 3, 4, 5],
   [4, 5, 1],
   [2, 5, 5]
 ]
-
 return: 35
 ------------------------------------------------------------------------------------------------ */
 const totalSum = (matrix) => {
@@ -74,13 +63,9 @@ const totalSum = (matrix) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
-
 You friend Pat has a chain of stores around the greater Seattle area. He specializes in selling salmon cookies. Pat has data for the hourly sales of cookies per hour for each store. He wants to create an array of the total number of cookies sold per hour for all of his stores combined.
-
 Write a function named grandTotal that adds up the cookies sales for each hour of operation for all of the stores combined. For example, the first element in the hourlySales array should be the sum of the cookies sold in the 9:00 a.m. hour at all five stores combined.
-
 For this example, the total at 9:00 a.m. is 17 + 26 + 7 + 5 + 33, or 88 total cookies.
-
 Return the array of the total number of cookies sold per hour for all of the stores combined.
 ------------------------------------------------------------------------------------------------ */
 
@@ -110,11 +95,8 @@ const grandTotal = (stores) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
-
 Pat has decided that he would also like to organize his data as objects containing the number of cookies sold per hour and the time.
-
 Here is sample data for the 9:00 sales: { sales: '88 cookies', time: '9 a.m.' }.
-
 Write a function named salesData that uses forEach to iterate over the hourlySales array and create an object for each hour. Return an array of the formatted data.
 ------------------------------------------------------------------------------------------------ */
 
@@ -132,7 +114,6 @@ const salesData = (hours, data) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
-
 Write a function named howManyTreats that will return the quantity of treats you need to pick up from the pet store today from this array.
 ------------------------------------------------------------------------------------------------ */
 
@@ -153,15 +134,23 @@ const errands = [
 
 const howManyTreats = (arr) => {
   // Solution code here...
+  let num=0;
+  arr.forEach(element => {
+    if (element.store === 'Pet store'){
+      element.items.forEach(x => {
+        if(x.name === 'Treats'){
+          num = x.quantity;
+        }
+      });
+    }
+  });
+  return num;
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
-
 Write a function named battleship that accepts a 2D array and two numbers: a row coordinate and a column coordinate.
-
 Return "hit" or "miss" depending on if there's part of a boat at that position in the array. Assume the array has only one of two values at each index. '#' for part of a boat, or ' ' for open water.
-
 Here is a sample board:
 [
   ['#', ' ', '#', ' '],
@@ -169,7 +158,6 @@ Here is a sample board:
   ['#', ' ', ' ', ' '],
   [' ', ' ', '#', '#'],
 ]
-
 The top row of the board is considered row zero and row numbers increase as they go down.
 ------------------------------------------------------------------------------------------------ */
 
@@ -179,9 +167,7 @@ const battleship = (board, row, col) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 8 - Stretch Goal
-
 Write a function named calculateProduct that takes in a two-dimensional array of numbers, multiplies all of the numbers in each array, and returns the final product. This function should work for any number of inner arrays.
-
 For example, the following input returns a product of 720: [[1,2], [3,4], [5,6]]
 ------------------------------------------------------------------------------------------------ */
 
@@ -191,9 +177,7 @@ const calculateProduct = (numbers) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 9 - Stretch Goal
-
 Write a function named averageDailyTemperature that accepts a two-dimensional array representing average daily temperatures grouped week-by-week.
-
 Calculate the average daily temperature during that entire period. Your output should be a single number. Write your function so it could accept an array with any number of weeks given to it.
 ------------------------------------------------------------------------------------------------ */
 
@@ -211,11 +195,8 @@ const averageDailyTemperature = (weather) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 10 - Stretch Goal
-
 Write a function named lowestWeeklyAverage that accepts a two-dimensional array of daily temperatures grouped week-by-week.
-
 Calculate the average temperature for each week and return the value of the lowest weekly average temperature.
-
 For example, in the data set below, the lowest weekly average is 46, which is the average of the temperatures in week 2. All other weeks have average temperatures that are greater than 46.
 ------------------------------------------------------------------------------------------------ */
 
@@ -232,13 +213,9 @@ const lowestWeeklyAverage = (weather) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 11 - Stretch Goal
-
 Write a function called excel that accepts a string representing rows and columns in a table.
-
 Rows are seperated by newline "\n" characters. Columns are seperated by commas. For example, '1,1,1\n4,4,4\n9,9,9' represents a 3x3 table.
-
 The function should parse the string as rows and columns and compute the sum of the values for each row. Return an array with the sum of the values in each row.
-
 For example, excel('1,1,1\n4,4,4\n9,9,9') returns [3, 12, 27].
 ------------------------------------------------------------------------------------------------ */
 
@@ -248,13 +225,9 @@ const excel = (str) => {
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
-
 All the code below will verify that your functions are working to solve the challenges.
-
 DO NOT CHANGE any of the below code.
-
 Run your tests from the console: jest challenge-12.test.js
-
 ------------------------------------------------------------------------------------------------ */
 
 describe('Testing challenge 1', () => {
