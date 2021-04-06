16f706535e54f5b46eff898003325ed15a4a7a46
haneenzyad98 <haneenzyad98@gmail.com>
2021-04-06 09:35:50 +0300

    8

--- code-challenges/challenges-08.test.js
+++ code-challenges/challenges-08.test.js
@@ -1,116 +1,96 @@
+  
 'use strict';
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1 - Review
-
-Write a function named returnTen, takes in a string and uses split
- and splice to return the last 10 characters from that string as 
- elements of an array.
-
+Write a function named returnTen, takes in a string and uses split and splice to return the last 10 characters from that string as elements of an array.
 ------------------------------------------------------------------------------------------------ */
 
 function returnTen(str){
   // Solution code here...
-  let arr=str.split('');
-  let arr2=arr.splice(arr.length-10);
-  return arr2;
+  let Array = str.split('');
+  let localArr = Array.splice(Array.length - 10);
+  return localArr;
 }
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
-
-Write a function named typeNum that, given an array as 
-input, uses filter to return an array containing only the
- numbers.
-
+Write a function named typeNum that, given an array as input, uses filter to return an array containing only the numbers.
 For example, typeNum([1, 'bob' ,3]) returns [1,3].
 ------------------------------------------------------------------------------------------------ */
 
 const typeNum = (arr) => {
   // Solution code here...
-  let x =arr.filter((itam)=>{
-    return (!isNaN(itam))
-  }
-  );
-  return x;
+  const localArr = arr.filter((element)=>{
+    return (!isNaN(element));
+  });
+  return localArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
-
-Write a function named containsAnd that, given an array of
- strings as input, uses filter to return an array containing
-  only strings that contain 'and' within the string.
-
-For example, containsAnd(['panda', 'ran', 'and']) returns
- ['panda', 'and'].
+Write a function named containsAnd that, given an array of strings as input, uses filter to return an array containing only strings that contain 'and' within the string.
+For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 ------------------------------------------------------------------------------------------------ */
 
 const containsAnd = (arr) => {
   // Solution code here...
-  let x =arr.filter(itam=>{
-    return itam.includes('and')
-  })
-  return x;
+ 
+   const localArr = arr.filter(element=>{
+    return element.includes('and');
+  });
+  return localArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
-
 Write a function named oddValues that, given an array of integers as input, uses filter to return an array containing only the odd integers.
-
 For example, oddValues([1,2,3]) returns [1,3].
 ------------------------------------------------------------------------------------------------ */
 
 const oddValues = (arr) => {
   // Solution code here...
-  let x =arr.filter(itam=>{
-    return(itam%2)
+  const localArr= arr.filter(element =>{
+    return (element%2);
   })
-  return x;
+  return localArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
-
-Write a function named filterStringsWithVowels that,
- given an array of strings as input, uses filter to return
-  an array with only words that contain vowels.
-
-The callback function to filter should include or utilize 
-a regular expression pattern.
-
-For example, filterStringsWithVowels('gregor','hound','xyz') 
-returns ['gregor', 'hound'].
+Write a function named filterStringsWithVowels that, given an array of strings as input, uses filter to return an array with only words that contain vowels.
+The callback function to filter should include or utilize a regular expression pattern.
+For example, filterStringsWithVowels('gregor','hound','xyz') returns ['gregor', 'hound'].
 ------------------------------------------------------------------------------------------------ */
 
 
 const filterStringsWithVowels = (arr) => {
   // Solution code here...
+  const regex= /[aeiou]+/mg;
+  const localArr = arr.filter(element=>{
+    return element.match(regex)
+  });
+  return localArr;
 };
 
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
-
 Write a function named notInFirstArray that, given two arrays as input, uses filter to return an array of all the elements in the second array that are not included in the first array.
-
 For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
 
 const notInFirstArray = (forbiddenValues, arr) => {
   // Solution code here...
-  let x = arr.filter(itam=>{
-    return !(forbiddenValues.includes(itam));
+  const localArr = arr.filter(element=>{
+    return !(forbiddenValues.includes(element));
   });
-  return x;
+  return localArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
-
 Write a function named getBaseStatGreaterThan that, given the snorlaxData, below, and an integer as input, uses filter to return an array containing all stats with a baseStat greater than the integer.
-
 For example, getBaseStatGreaterThan(snorlaxData.stats, 50) will return an array containing the 'special-defense' and 'special-attack' objects.
 ------------------------------------------------------------------------------------------------ */
 
@@ -151,9 +131,7 @@ const getBaseStatGreaterThan = (arr, minBaseStat) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 8 - Stretch Goal
-
 Write a function named getStatName that is an extension of your getBaseStatGreaterThan function from challenge 4. For this function, extend your solution from challenge 4 to only return the name of the stat, rather than the entire stat object.
-
 For example, getStatName(snorlaxData.stats, 50) will return ['special-defense', 'special-attack'].
 ------------------------------------------------------------------------------------------------ */
 
@@ -163,7 +141,6 @@ const getStatName = (arr, minBaseStat) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 9 - Stretch Goal
-
 Write a function named getCharactersWithoutChildren that, given the array of characters, below, uses filter to return an array of all characters without children.
 ------------------------------------------------------------------------------------------------ */
 
@@ -216,9 +193,7 @@ const getCharactersWithoutChildren = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 10 - Stretch Goal
-
 Write a function named evenOddNumericValues that, given an array as input, uses filter to remove any non-numeric values, then uses map to generate a new array containing the string 'even' or 'odd', depending on the original value.
-
 For example: evenOddNumericValues(['Gregor', 2, 4, 1]) returns ['even', 'even', 'odd'].
 ------------------------------------------------------------------------------------------------ */
 
@@ -228,13 +203,9 @@ const evenOddNumericValues = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
-
 All the code below will verify that your functions are working to solve the challenges.
-
 DO NOT CHANGE any of the below code.
-
 Run your tests from the console: jest challenges-08.test.js
-
 ------------------------------------------------------------------------------------------------ */
 
 describe('Testing challenge 1', () => {
