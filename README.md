ea0511bb84aaaf44f19931ba405958875677ffad
Haneen zyad odebat <haneenzyad98@gmail.com>
2021-05-29 22:54:58 +0300

    ch14

--- README.md
+++ README.md
@@ -47,3 +47,20 @@ function which takes in an array and the value to be added and  return an array 
       //  BinarySearch([4,8,15,23,42],23);
 
 ![img](img/ch03.jpeg)
+
+
+
+# Code Challenge: Class 15: Binary Tree and BST Implementation
+
+# Trees
+<!-- Short summary or background information -->
+ A tree is a nonlinear data structure, compared to arrays, linked lists, stacks and queues which are linear data structures. A tree can be empty with no nodes or a tree is a structure consisting of one node called the root and zero or one or more subtrees.
+
+## Challenge
+<!-- Description of the challenge -->
+Define a method for each of the depth first traversals called preOrder, inOrder, and postOrder ,add ,contains .
+
+![img](img/screenshot.jpeg)
+
+
+
---
+++ tree/__tests__/tree.test.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const BinaryTree = require('../tree').BinaryTree;
+const BinarySearchTree = require('../tree').BinarySearchTree;
+const Node = require('../tree').Node;
+
+let tree = null;
+describe('Binary Tree', ()=> {
+
+    beforeAll(()=> {
+        let one = new Node(1);
+        let two = new Node(2);
+        let three = new Node(3);
+        let four = new Node(4);
+        let five = new Node(5);
+        let six = new Node(6);
+        let seven = new Node(7);
+        let eight = new Node(8);
+        let nine = new Node(9);
+
+        one.left = two;
+        one.right = three;
+        two.left = six;
+        six.right = seven;
+        seven.left = eight;
+        seven.right = nine;
+        three.left = four;
+        three.right = five;
+
+        tree = new BinaryTree(one);
+    });
+
+    it ('constructor', ()=> {
+        let newTree = new BinaryTree();
+        expect(newTree.root).toBeNull();
+    });
+
+    it ('preOrder', ()=> {
+        let expected = [1, 2, 6, 7, 8, 9, 3, 4, 5];
+        let preOrderResult = tree.preOrder();
+        console.log("preOrderResult: ", preOrderResult)
+        expect(preOrderResult).toEqual(expected);
+    });
+
+    it ('inOrder', ()=> {
+        let expected = [6, 8, 7, 9, 2, 1, 4, 3, 5];
+        let inOrderResult = tree.inOrder();
+        console.log("inOrderResult: ", inOrderResult)
+        expect(inOrderResult).toEqual(expected);
+    });
+
+    it ('postOrder', ()=> {
+        let expected = [8, 9, 7, 6, 2, 4, 5, 3, 1];
+        let postOrderResult = tree.postOrder();
+        console.log("postOrderResult: ", postOrderResult)
+        expect(postOrderResult).toEqual(expected);
+    });
+
+    it ('instantiate a tree with a single root node', ()=> {
+        let newt = new BinarySearchTree();
+        newt.add(11)
+        expect(newt.root.value).toEqual(11);
+    });
+
+    it (' returne true or false if the tree contain value', ()=> {
+        let newt = new BinarySearchTree();
+        newt.add(1)
+        newt.add(2)
+        newt.add(3)
+       
+        expect(newt.contains(1)).toEqual(true);
+        expect(newt.contains(2)).toEqual(true);
+        expect(newt.contains(5)).toEqual(false);
+        expect(newt.contains(10)).toEqual(false);
+    });
+});
