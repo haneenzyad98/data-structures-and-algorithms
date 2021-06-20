3b82742c0312cc72a2dac861932b1ba6e363e95a
Haneen zyad odebat <haneenzyad98@gmail.com>
2021-06-20 10:54:28 +0300

    ch27

---
+++ MergeSort/MergeSort.js
@@ -0,0 +1,12 @@
+function merge(left, right) {
+    let sortedArr = []; 
+  
+    while (left.length && right.length) {
+      if (left[0] < right[0]) {
+        sortedArr.push(left.shift());
+      } else {
+        sortedArr.push(right.shift());
+      }
+    }
+  }
+  module.exports = merge;
